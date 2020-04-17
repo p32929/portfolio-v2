107a55b2c9d0b1efe939cc86740ce686696ea145
p32929 <p32929@yahoo.com>
2020-04-17 20:14:07 +0600

    Remove ignored files

---
+++ build/precache-manifest.5961b42696bb725c9a23481c6f8092e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81abdbde9a91c88a32ae78b9b086c24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac549d91bbfb64dd7fe",
+    "url": "/static/css/main.f50792b4.chunk.css"
+  },
+  {
+    "revision": "613ebd7d2c0e5e0fb060",
+    "url": "/static/js/2.fb3cb4d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fb3cb4d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac549d91bbfb64dd7fe",
+    "url": "/static/js/main.99078ee0.chunk.js"
+  },
+  {
+    "revision": "3dfe7464b8ec065c7890",
+    "url": "/static/js/runtime-main.ed2605bf.js"
+  },
+  {
+    "revision": "daeaf731dec30f7b361b6cffbbbc307f",
+    "url": "/static/media/avatar2.daeaf731.png"
+  }
+]);
