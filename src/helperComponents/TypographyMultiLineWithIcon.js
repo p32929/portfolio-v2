026557cbc291f6850a51b6376dccfe4ad5518712
p32929 <p32929@yahoo.com>
2020-04-18 10:52:41 +0600

    fixed about typography

--- src/helperComponents/TypographyMultiLineWithIcon.js
+++ src/helperComponents/TypographyMultiLineWithIcon.js
@@ -31,32 +31,33 @@ import {connect} from "react-redux";
 
 const TypographyMultiLineWithIcon = (props) => {
     return (
-        <Grid item xs style={{padding: 8}} container direction='row' alignItems='flex-start' alignContent='center'>
-            <SvgHelper
-                size={18}
-                color={primaryColor}
-                path='M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z'/>
+        <Grid item xs style={{padding: 8}} container direction='row'>
 
-            <Grid style={{
-                marginLeft: 8,
-                marginRight: 8
-            }} container item xs direction='column' justify='center'>
+            <Grid container item xs direction='row' justify='flex-start' alignItems='center'>
 
-                <Typography style={{
-                    fontSize: 16,
+                <SvgHelper
+                    styles={{
+                        marginLeft: 8,
+                        marginRight: 8
+                    }}
+                    size={18}
+                    color={primaryColor}
+                    path='M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z'/>
+
+                <Typography variant='h6' style={{
                     fontWeight: 'bold',
                     color: "#212121"
                 }}>{props.children}</Typography>
 
-                <Typography style={{
-                    fontSize: 14,
-                    color: "#757575",
-                    marginTop: 4,
-                    lineHeight: 1.25,
-                    paddingRight: 48,
-                }}>{props.desc}</Typography>
-
             </Grid>
+
+            <Typography variant='body2' style={{
+                color: "#212121",
+                lineHeight: 1.25,
+                marginLeft: 32,
+                paddingRight: 48,
+            }}>{props.desc}</Typography>
+
         </Grid>
     );
 };
