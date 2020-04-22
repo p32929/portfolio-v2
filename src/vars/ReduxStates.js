ad247f5a75429c76a9dcd4b5d0fee0b8111b2b9f
p32929 <p32929@yahoo.com>
2020-04-22 17:05:40 +0600

    new redux state

--- src/vars/ReduxStates.js
+++ src/vars/ReduxStates.js
@@ -27,26 +27,32 @@ import {primaryColor} from '../vars/MainItems'
 
 const SET_DRAWER_STATE = 'SET_DRAWER_STATE';
 const SET_PRIMARY_COLOR = 'SET_PRIMARY_COLOR';
+const SET_WITH_LOWER_THAN_BREAKPOINT = 'SET_PRIMARY_COLOR';
 
 var store = {
     primaryColor: primaryColor,
     drawerState: true, // Duh! True means open
+    widthLowerThanBreakpoint: false
 }
 
-export const reducer = createStore(function reducer(state = store, action) {
+export const reducer = createStore((state = store, action) => {
     switch (action.type) {
         case SET_DRAWER_STATE:
-            console.log(SET_PRIMARY_COLOR + " " + JSON.stringify(state))
             return {
                 ...state,
                 drawerState: action.state
             }
 
         case SET_PRIMARY_COLOR:
-            console.log(SET_PRIMARY_COLOR + " " + JSON.stringify(state))
             return {
                 ...state,
                 primaryColor: action.state
+            }
+
+        case SET_WITH_LOWER_THAN_BREAKPOINT:
+            return {
+                ...state,
+                widthLowerThanBreakpoint: action.state
             }
 
         default:
@@ -66,5 +72,12 @@ export const setPrimaryColor = (color) => {
     reducer.dispatch({
         type: SET_PRIMARY_COLOR,
         state: color
+    })
+}
+
+export const setWidthLowerThanBreakpoint = (isMatched) => {
+    reducer.dispatch({
+        type: SET_WITH_LOWER_THAN_BREAKPOINT,
+        state: isMatched
     })
 }
