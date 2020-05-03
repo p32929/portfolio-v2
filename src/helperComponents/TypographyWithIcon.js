de40b1d3dee5ce5aaa316dddc32e80bc4a42d863
p32929 <p32929@yahoo.com>
2020-05-03 12:52:29 +0600

    title text smaller

--- src/helperComponents/TypographyWithIcon.js
+++ src/helperComponents/TypographyWithIcon.js
@@ -36,10 +36,11 @@ const TypographyWithIcon = (props) => {
                 size={18}
                 color={primaryColor}
                 path='M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z'/>
+
             <Typography style={{
                 marginLeft: 8,
                 marginRight: 8,
-                fontSize: 14,
+                fontSize: 12,
                 fontWeight: 'bold',
                 color: "#757575"
             }}>{props.children}</Typography>
