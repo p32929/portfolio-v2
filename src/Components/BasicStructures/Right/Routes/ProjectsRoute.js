208f92df3670794e25a60b0058237991de94902a
p32929 <p32929@yahoo.com>
2020-06-20 14:06:56 +0600

    removed styles from projects

--- src/Components/BasicStructures/Right/Routes/ProjectsRoute.js
+++ src/Components/BasicStructures/Right/Routes/ProjectsRoute.js
@@ -4,17 +4,11 @@ import {useOvermind} from "../../../../Others/OvermindHelper";
 import {getTextAndList} from "../../../../Others/GlobalMethods";
 import {projects} from "../../../../Others/GlobalVariables";
 
-const styles = {
-    listGridParent: {
-        height: '100%',
-    }
-}
-
 const ProjectsRoute = () => {
     const {state, actions} = useOvermind()
 
     return (
-        <Grid spacing={1} style={styles.listGridParent} container xs item direction='row'>
+        <Grid spacing={1} container xs item direction='row'>
             {
                 projects.map((item) => {
                     return getTextAndList(item, 96);
