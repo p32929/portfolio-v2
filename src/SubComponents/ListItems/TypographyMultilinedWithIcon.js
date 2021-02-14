ef66be8252a79d9615d4ff74ec53aa9df367aa57
p32929 <p32929@yahoo.com>
2021-02-14 21:23:14 +0600

    fixed about font size

--- src/SubComponents/ListItems/TypographyMultilinedWithIcon.js
+++ src/SubComponents/ListItems/TypographyMultilinedWithIcon.js
@@ -24,15 +24,15 @@ SOFTWARE.
 
 import React from 'react';
 import Grid from "@material-ui/core/Grid";
-import {useOvermind} from "../../Utils/OvermindHelper";
+import { useOvermind } from "../../Utils/OvermindHelper";
 import SvgHelper from "../Helpers/SvgHelper";
 import Typography from "@material-ui/core/Typography";
 
 const TypographyMultilinedWithIcon = (props) => {
-    const {state, actions} = useOvermind()
+    const { state, actions } = useOvermind()
 
     return (
-        <Grid item xs style={{padding: 8}} container direction='row'>
+        <Grid item xs style={{ padding: 8 }} container direction='row'>
 
             <Grid container item xs direction='row' justify='flex-start' alignItems='center'>
 
@@ -43,7 +43,7 @@ const TypographyMultilinedWithIcon = (props) => {
                     }}
                     size={16}
                     color={state.primaryColor}
-                    path='M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z'/>
+                    path='M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z' />
 
                 <Typography variant='h6' style={{
                     fontWeight: 'bold',
@@ -54,9 +54,10 @@ const TypographyMultilinedWithIcon = (props) => {
 
             <Typography variant={state.belowSm ? 'body1' : 'h6'} style={{
                 color: "#212121",
-                lineHeight: 0.95,
+                lineHeight: state.belowSm ? 1 : 0.95,
                 marginLeft: 8,
                 paddingRight: 8,
+                fontSize: state.belowSm ? 18 : 19
             }}>{props.desc}</Typography>
 
         </Grid>
