37249bbda1d3d811637e1a1d351bebc109e3bafd
p32929 <p32929@yahoo.com>
2021-05-27 08:34:54 +0600

    links showing ok

--- src/Components/Left/NavigationLinks.tsx
+++ src/Components/Left/NavigationLinks.tsx
@@ -1,39 +1,60 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Grid, Theme } from "@material-ui/core";
+import { Button, Divider, Grid, Theme } from "@material-ui/core";
 import { useActions, useAppState } from '../../Overmind/OvermindHelper';
 import { GlobalVars } from '../../Others/GlobalVars';
+import { NavLink } from "react-router-dom";
 
 interface Props {
 
 }
 
 const getThemeObj = (theme: Theme) => {
-    return {}
+    return {
+        link: {
+            marginLeft: 8,
+            marginRight: 8,
+            textDecoration: 'none'
+        },
+        divider: {
+            height: 1,
+            margin: 16,
+            width: 90,
+            alignSelf: 'center'
+        },
+    }
 }
 
 const useStyles = makeStyles((theme: Theme) => (getThemeObj(theme)))
 
-const NavLinks: React.FC<Props> = (props) => {
+const NavigationLinks: React.FC<Props> = (props) => {
     const { } = useActions()
     const { } = useAppState()
 
     const classes = useStyles();
 
     return <Grid container item xs sm md lg xl direction='column' justify='center' alignContent='stretch'>
+        <Divider className={classes.divider} orientation="horizontal" flexItem />
+
         {
             GlobalVars.navLinks.map((item, index) => {
                 return <Button
+                    className={classes.link}
                     key={index}
                     onClick={() => {
 
                     }}
                     size='small'
+                // component={NavLink}
+                // to={item.toLowerCase()}
                 >{item}</Button>
 
             })
         }
+
+        <Divider className={classes.divider} orientation="horizontal" flexItem />
+
     </Grid>
 }
 
-export default NavLinks;
+export default NavigationLinks;
--- src/Components/LeftPart.tsx
+++ src/Components/LeftPart.tsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Theme, Grid, Paper } from "@material-ui/core";
 import { useActions, useAppState } from '../Overmind/OvermindHelper';
 import AvatarText from './Left/AvatarText';
-import NavLinks from './Left/NavLinks';
+import NavigationLinks from './Left/NavigationLinks';
 
 interface Props {
 
@@ -32,7 +32,7 @@ const LeftPart: React.FC<Props> = (props) => {
         <Paper elevation={12} className={classes.root}>
             <Grid className={classes.root} container direction="column" justify="space-around" alignContent='center' alignItems="center">
                 <AvatarText />
-                <NavLinks />
+                <NavigationLinks />
             </Grid>
         </Paper>
     </Grid>
