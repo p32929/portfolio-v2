4bfb2b6504bdf8aa72424bd95585690774612b16
p32929 <p32929@yahoo.com>
2021-05-30 16:33:12 +0600

    added gh pages

--- src/Components/Right/Top/TopRightButtons.tsx
+++ src/Components/Right/Top/TopRightButtons.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { Button, Divider, Grid, IconButton, Theme } from "@material-ui/core";
-import { useActions, useAppState } from '../../../Overmind/OvermindHelper';
+import {makeStyles} from '@material-ui/core/styles';
+import {Button, Divider, Grid, IconButton, Theme} from "@material-ui/core";
+import {useActions, useAppState} from '../../../Overmind/OvermindHelper';
 import SvgHelper from '../../../Helpers/SvgHelper';
-import { OurIcons } from '../../../Others/OurIcons';
+import {OurIcons} from '../../../Others/OurIcons';
 
 interface Props {
 
@@ -20,26 +20,35 @@ const getThemeObj = (theme: Theme) => {
 const useStyles = makeStyles((theme: Theme) => (getThemeObj(theme)))
 
 const TopRightButtons: React.FC<Props> = (props) => {
-    const { setPrimaryColor, showColorPickerDialog } = useActions()
-    const { } = useAppState()
+    const {setPrimaryColor, showColorPickerDialog} = useActions()
+    const {} = useAppState()
 
     const classes = useStyles();
 
     return <Grid container direction='row' justify='flex-end' item xs={4}>
+        {/*
+            PLEASE, DON'T CHANGE THIS GITHUB URL.
+            ( https://github.com/p32929/p32929.github.io/ )
+
+            YOU MAY USE THE SOURCE CODE HOWEVER YOU WANT, BUT PLEASE,
+            KEEP THE FORK-ICON & the GITHUB-URL under the icon UNCHANGED,
+            SO THAT OTHERS CAN ALSO BENIFIT FROM THE ORIGINAL REPOSITORY.
+            THANKS... :)
+        */}
         <IconButton size='small' target="_blank" href='https://github.com/p32929/p32929.github.io/'>
             <SvgHelper
-                styles={{ padding: 8 }}
+                styles={{padding: 8}}
                 path={OurIcons.fork}
-                size={20} />
+                size={20}/>
         </IconButton>
 
         <IconButton size='small' onClick={() => {
             showColorPickerDialog(true)
         }}>
             <SvgHelper
-                styles={{ padding: 8 }}
+                styles={{padding: 8}}
                 path={OurIcons.circle}
-                size={20} />
+                size={20}/>
         </IconButton>
     </Grid>
 
