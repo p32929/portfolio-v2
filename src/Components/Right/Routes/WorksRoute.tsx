1abb7b1946c621b4a854ea4d3a9265c5bb895e15
p32929 <p32929@yahoo.com>
2022-04-24 20:56:11 +0600

    fetch async

--- src/Components/Right/Routes/WorksRoute.tsx
+++ src/Components/Right/Routes/WorksRoute.tsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react'
-import {makeStyles} from '@material-ui/core/styles';
-import {Button, Divider, Grid, Theme} from "@material-ui/core";
-import {useActions, useAppState} from '../../../Overmind/OvermindHelper';
-import {GlobalVars, myGithubUsername} from '../../../Others/GlobalVars';
+import React, { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Divider, Grid, Theme } from "@material-ui/core";
+import { useActions, useAppState } from '../../../Overmind/OvermindHelper';
+import { GlobalVars, myGithubUsername } from '../../../Others/GlobalVars';
 import ListItemWithIcon from '../../Items/ListItemWithIcon';
-import {OurIcons} from "../../../Others/OurIcons";
-import {WebImages} from "../../../Others/Images";
-import {TitledListItemInterface} from "../../../Others/Interfaces";
+import { OurIcons } from "../../../Others/OurIcons";
+import { WebImages } from "../../../Others/Images";
+import { TitledListItemInterface } from "../../../Others/Interfaces";
 
 interface Props {
 
@@ -25,12 +25,42 @@ interface GithubResp {
 const useStyles = makeStyles((theme: Theme) => (getThemeObj(theme)))
 
 const WorksRoute: React.FC<Props> = (props) => {
-    const {} = useActions()
-    const {} = useAppState()
+    const { } = useActions()
+    const { } = useAppState()
     const [works, setWorks] = useState<Array<TitledListItemInterface>>([])
     const classes = useStyles();
 
+    const getGithubRepos = async () => {
+        const data = await fetch(`https://api.github.com/users/${myGithubUsername}/repos?per_page=999`)
+        const jsonFromFetch = await data.json()
+        const json: Array<GithubResp> = jsonFromFetch
+
+        var newWorks: Array<TitledListItemInterface> = [
+            ...GlobalVars.works
+        ]
+        var newWOrkObj: TitledListItemInterface = {
+            text: "Github repositories",
+            arr: []
+        }
+
+        for (var i = 0; i < json.length; i++) {
+            const obj = json[i]
+            newWOrkObj?.arr?.push({
+                title: obj.name,
+                logo: WebImages.giftIcon,
+                desc: obj.description ?? "Lots of amazingness",
+                link: obj.html_url
+            })
+        }
+        newWorks.push(newWOrkObj)
+        setWorks(newWorks)
+    }
+
     useEffect(() => {
+        if (myGithubUsername) {
+            getGithubRepos()
+        }
+
         if (myGithubUsername) {
             fetch(`https://api.github.com/users/${myGithubUsername}/repos?per_page=999`)
                 .then((data) => {
@@ -74,7 +104,7 @@ const WorksRoute: React.FC<Props> = (props) => {
     return <Grid container xs={12} item direction='row'>
         {
             getWorks().map((item, index) => {
-                return <ListItemWithIcon key={index} item={item}/>
+                return <ListItemWithIcon key={index} item={item} />
             })
         }
     </Grid>
