c70ab6225c68a39b191555bd093fe388427a5dbb
p32929 <p32929@yahoo.com>
2022-12-08 08:34:50 +0600

    updated package json

--- src/Others/GlobalVars.ts
+++ src/Others/GlobalVars.ts
@@ -14,7 +14,7 @@ export class GlobalVars {
         '#DD2C00'
     ]
     static myName = 'Fayaz Bin Salam'
-    static myOccupation = 'CEO at Rich IT'
+    static myOccupation = 'Software Engineer'
     static myDetails = 'Hello, I am Fayaz Bin Salam. I love Programming, making application/software for Mobile, Web and Desktop. For me, A Computer or A Mobile phone is nothing but some plastics and metals. But when it understands my language, it feels like a miracle!\nI started programming as a hobby since 2009. Wrote my first few scripts in Batch and VBScript. Later I learned C, C++, Java, Python, JavaScript etc, Worked in so many libraries & frameworks like ReactJS(one of my favorites), Angular2+, NodeJS, worked using so many technologies like Git, Arduino, etc. It does feel amazing to see myself improving. Hope to improve everything around me one day...'
     static myLeftAvatarImage = null
     static myRightAvatarImage = null
